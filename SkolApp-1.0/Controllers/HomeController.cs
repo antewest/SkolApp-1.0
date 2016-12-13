f5135a86dd3084ac41b023ff61ce31176b5ed85f
Kristoffer <krillllind@gmail.com>
2016-12-13 13:04:35 +0100

    Working word and image page

--- SkolApp-1.0/Controllers/HomeController.cs
+++ SkolApp-1.0/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace SkolApp_1._0.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult WordAndImages()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
