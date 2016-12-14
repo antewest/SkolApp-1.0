5a3f6d1333b00b7dca69891178c487c42152b0b7
MZDisaster <moaazw@gmail.com>
2016-12-14 08:34:38 +0100

    fixed punctuation.

--- SkolApp-1.0/SkolApp/controllers/punctuationController.js
+++ SkolApp-1.0/SkolApp/controllers/punctuationController.js
@@ -1,7 +1,10 @@
 ﻿
 SkolApp.controller("punctuationController", ["$scope", "$sce", function ($scope, $sce) {
     var punctuation = [".", "?", ",", "!"];
+
     $scope.rightsentence = "I have a hamster, a dog, and a cat.";
+
+    $scope.answersentence = "";
 
     $scope.astrisksentence = function () {
         var temp1 = $scope.rightsentence;
@@ -17,32 +20,26 @@ SkolApp.controller("punctuationController", ["$scope", "$sce", function ($scope,
         return temp2;
     };
 
-    $scope.answersentence = "";
-
     $scope.checkstring = function () {
-        var indexes = [];
-        var substrings = $scope.astrisksentence().split('*');
         var string = "";
-        if($scope.rightsentence == $scope.answersentence)
-        {
-            console.log('right');
-        } else {
-            for (var i = 0; i < $scope.rightsentence.length; i++) {
-                if ($scope.rightsentence[i] != $scope.answersentence[i] && punctuation.indexOf($scope.rightsentence[i]) != -1)
-                    indexes.push(i);
+        var newstring = [];
+
+        angular.forEach($scope.rightsentence, function (value, key, obj) {
+            if (obj[key] != $scope.answersentence[key] && punctuation.indexOf($scope.rightsentence[key]) != -1)
+            {
+                    this.push("<span class='orange'>" + value + "</span>");
             }
-            console.log(indexes);
-            for (var i = 0; i < (substrings.length) ; i++) {
-                if (substrings.length == indexes.length)
-                    string += substrings[i] + "<span class='orange'>" + $scope.rightsentence[indexes[i]] + "</span>";
-                else
-                {
-                    //string += 
-                    // to do;
-                }
+            else if (obj[key] == $scope.answersentence[key] && punctuation.indexOf($scope.answersentence[key]) != -1) {
+                this.push("<span class='green'>" + value + "</span>");
             }
-            console.log(string);
-        }
+            else
+                this.push(value);
+        }, newstring);
+
+        angular.forEach(newstring, function (value, key) {
+                string += value;
+        });
+        
         $scope.answersentenceView = string == "" ? $scope.rightsentence : string;
     };
 }]);
